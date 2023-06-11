0fe52402f23ae294678401fa96521339df51e55c
Floris Heyvaert <floris.heyvaert@gmail.com>
2023-06-11 21:22:21 +0200

    copy boilerplate

--- custom_components/restack/__init__.py
+++ custom_components/restack/__init__.py
@@ -1,71 +1,125 @@
-import logging
-import asyncio
-import aiohttp
+"""The ReStack integration."""
+from __future__ import annotations
 
-from .const import DOMAIN
-from homeassistant.helpers.aiohttp_client import async_create_clientsession
-from homeassistant.core import HomeAssistant
+from pyrestack import (
+    ReStack,
+    ReStackConnectionException,
+    ReStackException,
+    Stack,
+)
+
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_PASSWORD,
+    CONF_PORT,
+    CONF_USERNAME,
+    CONF_VERIFY_SSL,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-_LOGGER = logging.getLogger(__name__)
+from .const import COORDINATOR_UPDATE_INTERVAL, DOMAIN, LOGGER, PLATFORMS
 
-ATTR_NAME = "name"
-DEFAULT_NAME = "World"
-
-SERVICE_NAME = "execute"
-
-async def async_setup(hass: HomeAssistant, config: ConfigEntry):
-    """Set up is called when Home Assistant is loading our component."""
-
-    _LOGGER.info("Setting up")
-
-    session = async_create_clientsession(hass)
-    hass.data[DOMAIN] = {"session": session}
-
-    async def handle_execute(call):
-        """Handle the service call."""
-        # name = call.data.get(ATTR_NAME, DEFAULT_NAME)
-        # hass.states.async_set(f"{DOMAIN}.executed", name)
-
-        _LOGGER.info("Executed call starting")
-
-        hass.states.async_set(f"{DOMAIN}.executed", "calling")
-
-        try:
-            response = await asyncio.wait_for(
-                session.get(f"https://192.168.5.181:7146/WeatherForecast"),
-                timeout=5  # Timeout waarde in seconden
-            )
-            
-            _LOGGER.info("Executed call ended")
-
-            # Check the response status
-            if response.status == 200:
-                hass.states.async_set(f"{DOMAIN}.executed", "success")
-            else:
-                hass.states.async_set(f"{DOMAIN}.executed", "failed")
-                
-        except asyncio.TimeoutError:
-            _LOGGER.error("API call timed out")
-            hass.states.async_set(f"{DOMAIN}.executed", "timeout")
-        except Exception as e:
-            _LOGGER.error(f"Error occurred during API call: {str(e)}")
-            hass.states.async_set(f"{DOMAIN}.executed", "error")
-
-
-    _LOGGER.info("Registering services")
-
-    # Register services
-    hass.services.async_register(DOMAIN, SERVICE_NAME, handle_execute)
-
-    # Return boolean to indicate that initialization was successful.
-    return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Setup the config entry for my device."""
-    # device = MyDevice(entry.data[CONF_HOST])
-    # try:
-    #     await device.async_setup()
-    # except (asyncio.TimeoutError, TimeoutException) as ex:
-    #     raise ConfigEntryNotReady(f"Timeout while connecting to {device.ipaddr}") from ex
+    """Set up ReStack from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    host: str = entry.data[CONF_HOST]
+    port: int = entry.data[CONF_PORT]
+    username: str = entry.data[CONF_USERNAME]
+    password: str = entry.data[CONF_PASSWORD]
+    verify_ssl: bool = entry.data[CONF_VERIFY_SSL]
+    restack_api = ReStack(
+        async_get_clientsession(hass),
+        f"{host}:{port}",
+        username,
+        password,
+        verify_ssl,
+    )
+    dev_reg = dr.async_get(hass)
+    hass.data[DOMAIN][entry.entry_id] = coordinator = ReStackDataUpdateCoordinator(
+        hass,
+        config_entry_id=entry.entry_id,
+        dev_reg=dev_reg,
+        api=restack_api,
+    )
+
+    await coordinator.async_config_entry_first_refresh()
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
+
+
+class ReStackDataUpdateCoordinator(DataUpdateCoordinator):
+    """Data update coordinator for ReStack"""
+
+    data: list[Stack]
+    config_entry: ConfigEntry
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        config_entry_id: str,
+        dev_reg: dr.DeviceRegistry,
+        api: ReStack,
+    ) -> None:
+        """Initialize coordinator."""
+        super().__init__(
+            hass,
+            LOGGER,
+            name=DOMAIN,
+            update_interval=COORDINATOR_UPDATE_INTERVAL,
+        )
+        self._config_entry_id = config_entry_id
+        self._device_registry = dev_reg
+        self.api = api
+
+    async def _async_update_data(self) -> dict | None:
+        """Update data."""
+        try:
+            response = await self.api.async_get_stacks()
+        except ReStackConnectionException as exception:
+            raise UpdateFailed(exception) from exception
+        except ReStackException as exception:
+            raise UpdateFailed(exception) from exception
+
+        stacks: list[Stack] = response.data
+
+        current_stacks = {
+            list(device.identifiers)[0][1]
+            for device in dr.async_entries_for_config_entry(
+                self._device_registry, self._config_entry_id
+            )
+        }
+        new_stacks = {str(stack.name) for stack in stacks}
+        if stale_stacks := current_stacks - new_stacks:
+            for id in stale_stacks:
+                if device := self._device_registry.async_get_device(
+                    {(DOMAIN, id)}
+                ):
+                    self._device_registry.async_remove_device(device.id)
+
+        # If there are new stacks, we should reload the config entry so we can
+        # create new devices and entities.
+        if self.data and new_stacks - {
+            str(stack.name) for stack in self.data
+        }:
+            self.hass.async_create_task(
+                self.hass.config_entries.async_reload(self._config_entry_id)
+            )
+            return None
+
+        return stacks
+        # return await super()._async_update_data()
--- custom_components/restack/config_flow.py
+++ custom_components/restack/config_flow.py
@@ -1,31 +1,38 @@
+"""Config flow for ReStack integration."""
 from __future__ import annotations
 
 from typing import Any
+
+from pyrestack import ReStack, ReStackException
 import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import (
     CONF_HOST,
+    CONF_PASSWORD,
     CONF_PORT,
+    CONF_USERNAME,
     CONF_VERIFY_SSL,
 )
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import DOMAIN
+from .const import DOMAIN, LOGGER
 
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_HOST): str,
-        vol.Optional(CONF_PORT, default=5000): vol.Coerce(int),
+        vol.Optional(CONF_PORT, default=443): vol.Coerce(int),
+        vol.Optional(CONF_USERNAME, default=""): str,
+        vol.Optional(CONF_PASSWORD, default=""): str,
         vol.Optional(CONF_VERIFY_SSL, default=True): bool,
     }
 )
 
+
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Example config flow."""
-    # The schema version of the entries that it creates
-    # Home Assistant will call your migrate method if the version changes
+    """Handle a config flow for ReStack."""
+
     VERSION = 1
 
     async def _validate_input(
@@ -35,25 +42,27 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         errors: dict[str, str] = {}
         host: str = data[CONF_HOST]
         port: int = data[CONF_PORT]
+        username: str = data[CONF_USERNAME]
+        password: str = data[CONF_PASSWORD]
         verify_ssl: bool = data[CONF_VERIFY_SSL]
 
-        # uptime_robot_api = UptimeKuma(
-        #     async_get_clientsession(self.hass),
-        #     f"{host}:{port}",
-        #     username,
-        #     password,
-        #     verify_ssl,
-        # )
+        restack_api = ReStack(
+            async_get_clientsession(self.hass),
+            f"{host}:{port}",
+            username,
+            password,
+            verify_ssl,
+        )
 
-        # try:
-        #     await uptime_robot_api.async_get_monitors()
-        # except UptimeKumaException as exception:
-        #     LOGGER.error(exception)
-        #     errors["base"] = "cannot_connect"
-        # except Exception as exception:  # pylint: disable=broad-except
-        #     LOGGER.exception(exception)
-        #     errors["base"] = "unknown"
-        #     # return await self._show_setup_form(errors)
+        try:
+            await restack_api.async_get_stacks()
+        except ReStackException as exception:
+            LOGGER.error(exception)
+            errors["base"] = "cannot_connect"
+        except Exception as exception:  # pylint: disable=broad-except
+            LOGGER.exception(exception)
+            errors["base"] = "unknown"
+            # return await self._show_setup_form(errors)
 
         return errors
 
